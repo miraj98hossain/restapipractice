210cc3f8ea62d5cec118af9ad5ed35a5e1f26b90
miraj98hossain <miraj09hossain@gmail.com>
2025-03-24 22:55:23 +0600

    Securiy modified no authorization for any api

--- src/main/java/com/restApi/demo/restapipractice/config/SecurityConfig.java
+++ src/main/java/com/restApi/demo/restapipractice/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
-                request -> request.requestMatchers("/api/user/*").permitAll()
+                request -> request.requestMatchers("/api/user/**", "/api/emp/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
